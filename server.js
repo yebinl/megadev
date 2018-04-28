fdc29a034f9e6ce052875f3fa9db49205ef582e9
yebinl <szhome151@icloud.com>
2018-04-28 05:36:19 -0700

    fix blank page

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use(bodyParser.json());                           //bodyParser middleware
 app.use(passport.initialize());                       //passport middleware
 require('./config/passport')(passport);               //passport configuration
 
-//Server static assets if in production
-if(process.env.NODE_ENV == 'production') {
-  //set static folder
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
