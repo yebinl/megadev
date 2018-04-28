79f8be1d7bbff784932ec04762567c22989dbcc0
yebinl <szhome151@icloud.com>
2018-04-28 05:11:41 -0700

    fix 500 heroku problem by change sentFile to sendFile

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV == 'production') {
   //set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sentFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
